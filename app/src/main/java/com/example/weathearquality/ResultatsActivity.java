25766a85c87d1aaef404c901c5212ff309ec68a1
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-03-28 11:11:07 +0200

    Correction bug + affichage quand ville non trouvée + ajout langue anglais EU

--- app/src/main/java/com/example/weathearquality/ResultatsActivity.java
+++ app/src/main/java/com/example/weathearquality/ResultatsActivity.java
@@ -35,6 +35,9 @@ public class ResultatsActivity extends AppCompatActivity {
     private TextView tTemperature;
     private TextView tQualiteAir;
 
+    private TextView lTemperature;
+    private TextView lQualiteAir;
+
     String notePollution;
 
     public static final String TAG = "TAG";
@@ -59,7 +62,8 @@ public class ResultatsActivity extends AppCompatActivity {
         tVille = findViewById(R.id.ville_resultats);
         tTemperature = findViewById(R.id.temperature_resultats);
         tQualiteAir = findViewById(R.id.qualite_air_resultats);
-
+        lTemperature = findViewById(R.id.temperature_label);
+        lQualiteAir = findViewById(R.id.qualite_air_label);
         // Récupération des données passées en paramètre
         Intent intent = getIntent();
         String ville = intent.getStringExtra("ville");
@@ -80,82 +84,85 @@ public class ResultatsActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<JsonElement> call, Response<JsonElement> response) {
                 if (response.isSuccessful()) {
-                    JsonElement jsonElement = response.body();
-                    JsonObject jsonGlobal = jsonElement.getAsJsonObject();
-
-
-                    JsonObject jsonData = jsonGlobal.getAsJsonObject("data");
-                    JsonObject jsonIaqi = jsonData.getAsJsonObject("iaqi");
-                    JsonObject jsonTemperature = jsonIaqi.getAsJsonObject("t");
-                    String temperature = jsonTemperature.get("v").getAsString();
-
-                    tTemperature.setText(temperature + " °C");
-
-                    notePollution = jsonData.get("aqi").getAsString();
-                    tQualiteAir.setText(notePollution);
-
-
                     View rectangle = findViewById(R.id.rectangle_resultat);
                     TextView rectangleText = findViewById(R.id.rectangle_text);
                     TextView implicationSante = findViewById(R.id.implicationSante);
                     TextView conseil = findViewById(R.id.conseil);
+                    JsonElement jsonElement = response.body();
+                    JsonObject jsonGlobal = jsonElement.getAsJsonObject();
+                    String jsonStatus = jsonGlobal.get("status").getAsString();
+                    if (jsonStatus.equals("ok")) {
+                        JsonObject jsonData = jsonGlobal.getAsJsonObject("data");
+                        JsonObject jsonIaqi = jsonData.getAsJsonObject("iaqi");
+                        JsonObject jsonTemperature = jsonIaqi.getAsJsonObject("t");
+                        String temperature = jsonTemperature.get("v").getAsString();
 
-                    int note= Integer.parseInt(notePollution);
-                    if (note < 50) {
-                        rectangle.setBackgroundColor(ContextCompat.getColor( context, R.color.green));
-                        rectangleText.setText(getString(R.string.QABon));
-                        implicationSante.setText(implicationSante.getText() + getString(R.string.HIBon));
-                        conseil.setText(conseil.getText() + getString(R.string.CSBon));
+                        tTemperature.setText(temperature + " °C");
 
-                    } else if (note < 100) {
-                        rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.yellow));
-                        rectangleText.setText(getString(R.string.QAMoyen));
-                        implicationSante.setText(implicationSante.getText() + getString(R.string.HIMoyen));
-                        conseil.setText(conseil.getText() + getString(R.string.CSMoyen));
+                        notePollution = jsonData.get("aqi").getAsString();
+                        tQualiteAir.setText(notePollution);
 
-                    } else if (note < 150) {
-                        rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.orange));
-                        rectangleText.setText(getString(R.string.QAMauvais));
-                        implicationSante.setText(implicationSante.getText() + getString(R.string.HIMauvais));
-                        conseil.setText(conseil.getText() + getString(R.string.CSMoyen));
 
-                    } else if (note < 200) {
-                        rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.red));
-                        rectangleText.setText(getString(R.string.QATresMauvais));
-                        implicationSante.setText(implicationSante.getText() + getString(R.string.HITresMauvais));
-                        conseil.setText(conseil.getText() + getString(R.string.HIMoyen));
 
-                    } else if (note < 300) {
-                        rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.purple));
-                        rectangleText.setText(getString(R.string.QADangereux));
-                        implicationSante.setText(implicationSante.getText() + getString(R.string.CSDangereux));
 
+                        int note = Integer.parseInt(notePollution);
+                        if (note < 50) {
+                            rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.green));
+                            rectangleText.setText(getString(R.string.QABon));
+                            implicationSante.setText(implicationSante.getText() + getString(R.string.HIBon));
+                            conseil.setText(conseil.getText() + getString(R.string.CSBon));
+
+                        } else if (note < 100) {
+                            rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.yellow));
+                            rectangleText.setText(getString(R.string.QAMoyen));
+                            implicationSante.setText(implicationSante.getText() + getString(R.string.HIMoyen));
+                            conseil.setText(conseil.getText() + getString(R.string.CSMoyen));
+
+                        } else if (note < 150) {
+                            rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.orange));
+                            rectangleText.setText(getString(R.string.QAMauvais));
+                            implicationSante.setText(implicationSante.getText() + getString(R.string.HIMauvais));
+                            conseil.setText(conseil.getText() + getString(R.string.CSMoyen));
+
+                        } else if (note < 200) {
+                            rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.red));
+                            rectangleText.setText(getString(R.string.QATresMauvais));
+                            implicationSante.setText(implicationSante.getText() + getString(R.string.HITresMauvais));
+                            conseil.setText(conseil.getText() + getString(R.string.HIMoyen));
+
+                        } else if (note < 300) {
+                            rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.purple));
+                            rectangleText.setText(getString(R.string.QADangereux));
+                            implicationSante.setText(implicationSante.getText() + getString(R.string.CSDangereux));
+
+                        } else {
+                            rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.black));
+                            rectangleText.setText(getString(R.string.QATresDangereux));
+                            rectangleText.setTextColor(ContextCompat.getColor(context, R.color.white));
+                            implicationSante.setText(implicationSante.getText() + getString(R.string.HITresDangereux));
+                            conseil.setText(conseil.getText() + getString(R.string.CSDangereux));
+
+                        }
+
+                        //Insertion dans la base de données de la ville et de la date actuelle dans la table historique
+                        //Insertion des valeurs dans la base de données
+                        ContentValues valuesHistorique = new ContentValues();
+                        valuesHistorique.put("utilisateur", utilisateur);
+                        valuesHistorique.put("ville", ville);
+                        //Insertion la date dans la forme mm/dd/yy hh:mm
+                        Date date = new Date();
+                        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yy hh:mm");
+                        String formattedDate = formatter.format(date);
+                        valuesHistorique.put("date", formattedDate);
+                        bd.insert("historique", null, valuesHistorique);
                     } else {
-                        rectangle.setBackgroundColor(ContextCompat.getColor(context, R.color.black));
-                        rectangleText.setText(getString(R.string.QATresDangereux));
-                        rectangleText.setTextColor(ContextCompat.getColor(context, R.color.white));
-                        implicationSante.setText(implicationSante.getText() + getString(R.string.HITresDangereux));
-                        conseil.setText(conseil.getText() + getString(R.string.CSDangereux));
-
+                        rectangleText.setText(getString(R.string.VilleNonTrouvee));
+                        //Ne plus afficher les informations inutiles
+                        lTemperature.setVisibility(View.GONE);
+                        lQualiteAir.setVisibility(View.GONE);
+                        implicationSante.setVisibility(View.GONE);
+                        conseil.setVisibility(View.GONE);
                     }
-
-
-
-
-
-
-
-                    //Insertion dans la base de données de la ville et de la date actuelle dans la table historique
-                    //Insertion des valeurs dans la base de données
-                    ContentValues valuesHistorique = new ContentValues();
-                    valuesHistorique.put("utilisateur", utilisateur);
-                    valuesHistorique.put("ville", ville);
-                    //Insertion la date dans la forme mm/dd/yy hh:mm
-                    Date date = new Date();
-                    SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yy hh:mm");
-                    String formattedDate = formatter.format(date);
-                    valuesHistorique.put("date", formattedDate);
-                    bd.insert("historique", null, valuesHistorique);
                 }
             }
 
@@ -168,7 +175,7 @@ public class ResultatsActivity extends AppCompatActivity {
         // Affichage des données
         tVille.setText(ville);
         if (!afficherTemperature) {
-            TextView lTemperature =  findViewById(R.id.temperature_label);
+
             lTemperature.setVisibility(View.GONE);
             tTemperature.setVisibility(View.GONE);
         }
@@ -176,15 +183,17 @@ public class ResultatsActivity extends AppCompatActivity {
         // Affichage rectangle en fonction du resultat de la qualite de l'air
 
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflaterMenu  = getMenuInflater();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflaterMenu = getMenuInflater();
         inflaterMenu.inflate(R.menu.menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
 
             case R.id.histMenu:
                 Intent intentHist = new Intent(ResultatsActivity.this, HistoriqueActivity.class);
